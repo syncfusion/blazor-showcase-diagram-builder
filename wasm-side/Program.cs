d6b825b233b20dfe14fdb825f220aff1b90ba1a0
RajeswariSF3279 <rajeswari.p@syncfusion.com>
2022-02-16 19:55:41 +0530

    feature(BLAZ-20612): wasm code changes has been updated

---
+++ wasm-side/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.Blazor;
+using WebApplicationDiagramBuilder.Shared;
+
+
+namespace WebApplicationDiagramBuilder
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSyncfusionBlazor();
+            builder.Services.AddScoped<SampleService>();
+            await builder.Build().RunAsync();
+        }
+    }
+}
